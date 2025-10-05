382b97f8d967d7f7cf8c6d65aa9b5054e0d193fd
Mihai Andrei <mihai.andrei1597@gmail.com>
2025-10-05 15:20:02 +0300

    add new session approach

--- setup.sql
+++ setup.sql
@@ -8,7 +8,9 @@ CREATE TABLE user (
 CREATE INDEX github_id_index ON user(github_id);
 
 CREATE TABLE session (
-    id TEXT NOT NULL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES user(id),
-    expires_at INTEGER NOT NULL
+	id TEXT NOT NULL PRIMARY KEY,
+	secret_hash BLOB NOT NULL, -- blob is a SQLite data type for raw binary
+	created_at INTEGER NOT NULL, -- unix time (seconds)
+    last_verified_at INTEGER NOT NULL, -- unix (seconds)
+    user_id INTEGER NOT NULL REFERENCES user(id)
 );
